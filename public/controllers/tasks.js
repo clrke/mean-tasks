af50e770c07b2b13dffcc21df9a9cb6600496c11
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-05-02 23:10:24 +0800

    Reindent client tasks controller

--- public/controllers/tasks.js
+++ public/controllers/tasks.js
@@ -2,48 +2,48 @@
 
 /* jshint -W098 */
 angular.module('mean.tasks').controller('TasksController', ['$scope', '$location', 'Global', 'Tasks',
-  function($scope, $location, Global, Tasks) {
-    $scope.global = Global;
-    $scope.package = {
-      name: 'tasks'
-    };
-    $scope.tasksTab = 0;
+	function($scope, $location, Global, Tasks) {
+		$scope.global = Global;
+		$scope.package = {
+			name: 'tasks'
+		};
+		$scope.tasksTab = 0;
 
-    $scope.filterByTab = function () {
-    	if($scope.tasksTab == 2)
-    		return null;
+		$scope.filterByTab = function () {
+			if($scope.tasksTab == 2)
+				return null;
 
-    	return {
-    		done: $scope.tasksTab == 1
-    	}
-    }
-    $scope.hasAuthorization = function(article) {
-      if (!article || !article.user) return false;
-      return $scope.global.isAdmin || article.user._id === $scope.global.user._id;
-    };
+			return {
+				done: $scope.tasksTab == 1
+			}
+		}
+		$scope.hasAuthorization = function(article) {
+			if (!article || !article.user) return false;
+			return $scope.global.isAdmin || article.user._id === $scope.global.user._id;
+		};
 
-    $scope.create = function(isValid) {
-      if (isValid) {
-        var task = new Tasks({
-          title: this.title,
-          content: 'New Content',
-          done: false
-        });
-        task.$save(function(response) {
-          $location.path('tasks/' + response._id);
-        });
+		$scope.create = function(isValid) {
+			if (isValid) {
+				var task = new Tasks({
+					title: this.title,
+					content: 'New Content',
+					done: false
+				});
+				task.$save(function(response) {
+					$location.path('tasks/' + response._id);
+				});
 
-        this.title = '';
-        this.content = '';
-      } else {
-        $scope.submitted = true;
-      }
-    };
+				this.title = '';
+				this.content = '';
+			} else {
+				$scope.submitted = true;
+			}
+		};
 
-    $scope.find = function() {
-      Tasks.query(function(tasks) {
-        $scope.tasks = tasks;
-      });
-    };
-  }
-]);
+		$scope.find = function() {
+			Tasks.query(function(tasks) {
+				$scope.tasks = tasks;
+			});
+		};
+	}
+	]);
